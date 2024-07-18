03b129ee4fd88e1c6e94c605da56173b369d1377
Gokul <122973682+Gblnn@users.noreply.github.com>
2024-07-18 17:21:03 +0400

    first update

--- src/components/index-component.tsx
+++ src/components/index-component.tsx
@@ -17,7 +17,7 @@ export default function IndexComponent(props:Props){
         <div style={{padding:"1.5rem", background:"linear-gradient(rgba(18 18 80/ 65%), rgba(100 100 100/ 0%))", height:"90svh", border:""}}>
 
             <motion.div initial={{opacity:0}} whileInView={{opacity:1}}>
-                <Back icon={<Indent color="salmon"/>} title="Index" noback extra={<button onClick={()=>window.location.reload()} style={{paddingLeft:"1rem", paddingRight:"1rem", fontSize:"0.8rem"}}><RefreshCwIcon color="crimson" width={"1rem"}/>Update<p style={{opacity:0.5, letterSpacing:"0.15rem"}}>{props.version}</p></button>}/>
+                <Back icon={<Indent color="salmon"/>} title="CarLog" noback extra={<button onClick={()=>window.location.reload()} style={{paddingLeft:"1rem", paddingRight:"1rem", fontSize:"0.8rem"}}><RefreshCwIcon color="crimson" width={"1rem"}/>Update<p style={{opacity:0.5, letterSpacing:"0.15rem"}}>{props.version}</p></button>}/>
                 <br/>
 
                 <div style={{display:"flex", flexFlow:"column", gap:"0.5rem"}}>
--- src/pages/vehicles.tsx
+++ src/pages/vehicles.tsx
@@ -1,8 +1,9 @@
 import DbComponent from "@/components/database-component";
+import { Cloud } from "lucide-react";
 
 
 export default function Vehicles(){
     return(
-        <DbComponent title="Vehicles"/>
+        <DbComponent title="Vehicles" loader={<Cloud color="dodgerblue" width={"3rem"} height={"3rem"} style={{position:"absolute"}} className="animate-ping"/>}/>
     )
 }
