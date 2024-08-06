3c5ebb9e3c9a24b9cc688a9ebf11da379e99e647
Gokul <122973682+Gblnn@users.noreply.github.com>
2024-08-06 12:58:20 +0400

    Update maintenance.tsx

--- src/pages/maintenance.tsx
+++ src/pages/maintenance.tsx
@@ -2,8 +2,9 @@ import DbComponent from "@/components/database-component";
 import { Wrench } from "lucide-react";
 
 
+
 export default function Maintenance(){
     return(
-        <DbComponent db="maintenance" title="Logs" addItemTitle="Add Log" addItemIcon={<Wrench color="dodgerblue" width={"1rem"} />} loader={<Wrench color="dodgerblue" width={"3rem"} height={"3rem"} style={{position:"absolute"}} className="animate-ping"/>} type="log"/>
+        <DbComponent dbID="" db="maintenance" title="Logs" addItemTitle="Add Log" addItemIcon={<Wrench color="dodgerblue" width={"1rem"} />} loader={<Wrench color="dodgerblue" width={"3rem"} height={"3rem"} style={{position:"absolute"}} className="animate-ping"/>} type="log"/>
     )
 }
